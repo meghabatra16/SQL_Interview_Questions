e28c1ad99eabd5bb322f9da1ef54b094d123ab79
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2022-02-06 18:44:11 -0500

    Update Easy_Questions.sql

--- Easy_Questions.sql
+++ Easy_Questions.sql
@@ -6,8 +6,7 @@ where c.id = o.cust_id
 group by c.id, c.first_name
 order by c.first_name
 
-##2. Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
-You may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
+##2. Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details. you may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
 
 select c.first_name, c.last_name, c.city, o.order_details 
 from customers as c
