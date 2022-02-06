899831ffb746ac641ef6050b03ac2ccc3a9e3e43
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2022-02-06 18:43:27 -0500

    Update Easy_Questions.sql

--- Easy_Questions.sql
+++ Easy_Questions.sql
@@ -6,4 +6,26 @@ where c.id = o.cust_id
 group by c.id, c.first_name
 order by c.first_name
 
-##2. 
+##2. Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
+You may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
+
+select c.first_name, c.last_name, c.city, o.order_details 
+from customers as c
+left join orders as o
+on o.cust_id=c.id
+order by c.first_name, o.order_details
+
+##3. Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments. Output just the absolute difference in salaries.
+
+select ((select max(e.salary) 
+from db_employee as e
+inner join db_dept as d
+on e.department_id = d.id 
+where d.department = 'marketing') -
+(select max(e.salary) 
+from db_employee as e
+inner join db_dept as d
+on e.department_id = d.id 
+where d.department = 'engineering')) as difference
+
+##4. 
